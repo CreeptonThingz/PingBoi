b5229c47e669bdcab3a2b0973ec96525eae6a381
Jasper Shen <31055232+CreeptonThingz@users.noreply.github.com>
2021-10-29 16:06:15 +0000

    Fix improper imports

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const keepAlive = require('./server');
+const keepAlive = require('./server.js');
 
 const prefix = '.';
 const ownerID = "162672579025436673";
@@ -38,11 +38,11 @@ client.on('message', async message => {
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if (command === "help") { client.commands.get("help").execute(message, Discord); }
+    if (command === "help") { client.commands.get("help").execute(message); }
     
     if (command === "expurosion") { client.commands.get("expurosion").execute(message, isDisabled, setDisabled, ownerID); }
     if (command === "plotarmor") { client.commands.get("plotarmor").execute(message, isDisabled, setDisabled, ownerID);}
-    if (command === "monthlypfp") { client.commands.get("monthlypfp").execute(message, args, ownerID, getRandomInt); }
+    if (command === "monthlypfp") { client.commands.get("monthlypfp").execute(message, args, ownerID); }
 
     if (command === "ping") { client.commands.get("ping").execute(message, client); }
     if (command === "disabled") { client.commands.get("disabled").execute(message, isDisabled); }
@@ -51,13 +51,14 @@ client.on('message', async message => {
     if (!isDisabled) {
         if (command === "spam") { client.commands.get("spam").execute(message, args, isPinging, setPinging, setSpamVictim, setSpamStarter, setMentionMessage, setSpamPing, client); }
         if (command === "stop") { client.commands.get("stop").execute(message, isPinging, setPinging, spamVictim, spamStarter, ownerID, spamCount, spamPing, client); }
-        if (command === "image") { client.commands.get("image").execute(message, args, getRandomInt, Discord); }
+        if (command === "image") { client.commands.get("image").execute(message, args); }
         if (command === "say") { client.commands.get("say").execute(message, args); }
-        if (command === "idiot") { client.commands.get("idiot").execute(message, args, getRandomInt); }
+        if (command === "idiot") { client.commands.get("idiot").execute(message, args); }
         if (command === "ubw") { client.commands.get("ubw").execute(message); }
-        if (command === "megumin") { client.commands.get("megumin").execute(message, getRandomInt); }
-        if (command === "rngball") { client.commands.get("rngball").execute(message, getRandomInt, randomColor, Discord); }
-        if (command === "random") { client.commands.get("random").execute(message, args, getRandomInt, getRandomIntMin); }
+        if (command === "megumin") { client.commands.get("megumin").execute(message); }
+        if (command === "rngball") { client.commands.get("rngball").execute(message, Discord); }
+        if (command === "random") { client.commands.get("random").execute(message, args); }
+        if (command === "module") { console.log(module); }
     }
 });
 
@@ -101,6 +102,12 @@ function getRandomInt(max) {
 function randomColor() {
     return Math.floor(Math.random()*16777215).toString(16);
 }
+
+module.exports = {
+    getRandomIntMin: getRandomIntMin,
+    getRandomInt: getRandomInt,
+    randomColor: randomColor
+};
 
 keepAlive();
 client.login(process.env['logintoken']);
