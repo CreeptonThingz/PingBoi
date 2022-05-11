18e18f7d70617eea7168bc803036b911bbf47fde
Jasper Shen <31055232+CreeptonThingz@users.noreply.github.com>
2022-05-11 04:25:46 +0000

    Add spam count to pinging.js

--- commands/pinging.js
+++ commands/pinging.js
@@ -1,9 +1,10 @@
 function execute(message) {
     const bot = require("./../bot");
+    const spam = require("./spam");
 
     let status = bot.isPinging ? "PingBoi is currently pinging someone" : "PingBoi is currently not pinging someone";
 
-    message.channel.send(status);
+    message.channel.send(status + "\n" + "Currently at: " + spam.spamCount + " pings");
 }
 
 module.exports = {
