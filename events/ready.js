2f837673a5f7f42100da0d6fe88b91e63d865587
CreeptonThingz <jaspershen21@gmail.com>
2024-07-08 17:48:23 -0600

    Update activity status

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,21 @@
 // When the client is ready, run this code (only once).
-const { Events } = require("discord.js");
+const { Events, PresenceUpdateStatus, ActivityType } = require("discord.js");
+
+const defaultStatus = "the gacha system not giving me a S-Rank";
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		client.user.setPresence({
+			activities: [
+				{
+					name: defaultStatus,
+					type: ActivityType.Watching,
+				},
+			],
+			status: PresenceUpdateStatus.Online,
+		});
 	},
 };
