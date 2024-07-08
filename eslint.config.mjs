84ce23afd4a80c4b3fac5b52ef0bcc7a5f5efe3b
CreeptonThingz <jaspershen21@gmail.com>
2024-07-07 23:08:54 -0600

    Add prettier and eslint config files

---
+++ eslint.config.mjs
@@ -0,0 +1,52 @@
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends("eslint:recommended"),
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+
+    rules: {
+      "handle-callback-err": "off",
+      "max-nested-callbacks": [
+        "error",
+        {
+          max: 4,
+        },
+      ],
+      "no-console": "off",
+      "no-empty-function": "error",
+      "no-lonely-if": "error",
+      "no-shadow": [
+        "error",
+        {
+          allow: ["err", "resolve", "reject"],
+        },
+      ],
+      "no-var": "error",
+      "prefer-const": "error",
+      "spaced-comment": "error",
+      yoda: "error",
+    },
+  },
+];
